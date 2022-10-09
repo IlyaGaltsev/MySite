90539e342ff9a1b8b3580ef43636b33b6c8dbd0d
Илья Гальцев <galsev_i@inbox.ru>
2022-10-09 21:14:16 +0300

    vesion 0.3.2

--- src/components/SectionAbout/SectionAbout.jsx
+++ src/components/SectionAbout/SectionAbout.jsx
@@ -2,17 +2,18 @@ import './SectionAbout.css';
 import handle from '../../assets/icons/handle.png'
 import PrimaryButton from '../PrimaryButton/PrimaryButton';
 import buttons from '../../data/buttons';
+
 function SectionAbout() {
   return (
     <div className="section-about__wrapper" id='about'>
       <div className="section-about__title">
         <h1>
-        Привет! Я <span className='primary-color'>Илья</span>
+        Привет, я <span className='primary-color'>Илья</span>
         </h1>
         <img className='title__icon' src={handle} alt='k'/>
       </div>
       <h1>Я Frontend разработчик</h1>
-      <p className='section-about__text'>Специализируюсь на веб- и мобильной разработке с акцентом на разработку интерфейса. Заинтересован в создании чего-то потрясающего с помощью кода. Не так давно возникло желание делать более купные проекты, тем самым развиваться в разработке приложений и помогать людям, делая их жизнь чуть проще</p>
+      <p className='section-about__text'>Специализируюсь на мобильной и веб-разработке с акцентом на разработку интерфейса. Заинтересован в создании чего-то потрясающего с помощью кода. Не так давно возникло желание делать более купные проекты, тем самым развиваться в разработке приложений и помогать людям, делая их жизнь чуть проще</p>
       <div className="section-about__button">
         <PrimaryButton button={buttons[0]} />
       </div>
